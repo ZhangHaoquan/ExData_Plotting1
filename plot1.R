83e57dcb1110bb2a5af220683cd7ba17a9dbcbe7
ZhangHaoquan <stanley_zhang1989@hotmail.com>
2015-02-08 16:30:51 +0800

    1st commit

---
+++ plot1.R
@@ -0,0 +1,40 @@
+## This file should be reproducible in Windows R 3.1.2
+## W4Ptn32dc3jF9h8h0BW8N3GSF5IGxB7J is a unique key that tags all objects produced by this series of R codes
+
+## Data Downloading/Unzipping - If file extraction done previously, ignore
+if(!file.exists("household_power_consumption_W4Ptn32dc3jF9h8h0BW8N3GSF5IGxB7J.txt"))
+{
+	fileUrl <- "http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+	download.file(fileUrl, destfile = "./DownloadedDataFile.zip")
+	unzip("./DownloadedDataFile.zip")
+	file.rename("household_power_consumption.txt","household_power_consumption_W4Ptn32dc3jF9h8h0BW8N3GSF5IGxB7J.txt")
+}
+
+## Data Reading - If environment contains variable, ignore
+if(!("hse_pwr_con_W4Ptn32dc3jF9h8h0BW8N3GSF5IGxB7J" %in% ls()))
+{
+	hse_pwr_con_W4Ptn32dc3jF9h8h0BW8N3GSF5IGxB7J <- read.table("./household_power_consumption_W4Ptn32dc3jF9h8h0BW8N3GSF5IGxB7J.txt",
+		na.strings='?', sep=";", quote="", header=TRUE, colClasses = c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric") )
+}
+
+## Data reshaping
+if(!("sub_hse_pwr_con_W4Ptn32dc3jF9h8h0BW8N3GSF5IGxB7J" %in% ls()))
+{	
+	## Preserve the integrity/rawness of hse_pwr_con_W4Ptn32dc3jF9h8h0BW8N3GSF5IGxB7J by changing a variable
+	hse_pwr_con <- hse_pwr_con_W4Ptn32dc3jF9h8h0BW8N3GSF5IGxB7J
+
+	hse_pwr_con$DateTime <- strptime(paste(hse_pwr_con$Date, hse_pwr_con$Time), "%d/%m/%Y %H:%M:%S")
+	Start_Date <- strptime("2007-02-01", "%Y-%m-%d")
+	End_Date <- strptime("2007-02-03", "%Y-%m-%d")
+	sub_hse_pwr_con_W4Ptn32dc3jF9h8h0BW8N3GSF5IGxB7J <- hse_pwr_con[((hse_pwr_con$DateTime >= Start_Date) & (hse_pwr_con$DateTime <= End_Date)),]
+
+	## Preserve the integrity/rawness of sub_hse_pwr_con_W4Ptn32dc3jF9h8h0BW8N3GSF5IGxB7J by changing a variable
+	sub_hse_pwr_con <- sub_hse_pwr_con_W4Ptn32dc3jF9h8h0BW8N3GSF5IGxB7J	
+}
+
+
+## Plotting Plot 1
+png(file="plot1.png")
+hist(sub_hse_pwr_con$Global_active_power, col='Red', ylab='Frequency', xlab='Global Active Power (kilowatts)', main='Global Active Power')
+dev.off()
+
---
+++ plot4.R
@@ -0,0 +1,54 @@
+## This file should be reproducible in Windows R 3.1.2
+## W4Ptn32dc3jF9h8h0BW8N3GSF5IGxB7J is a unique key that tags all objects produced by this series of R codes
+
+## Data Downloading/Unzipping - If file extraction done previously, ignore
+if(!file.exists("household_power_consumption_W4Ptn32dc3jF9h8h0BW8N3GSF5IGxB7J.txt"))
+{
+	fileUrl <- "http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+	download.file(fileUrl, destfile = "./DownloadedDataFile.zip")
+	unzip("./DownloadedDataFile.zip")
+	file.rename("household_power_consumption.txt","household_power_consumption_W4Ptn32dc3jF9h8h0BW8N3GSF5IGxB7J.txt")
+}
+
+## Data Reading - If environment contains variable, ignore
+if(!("hse_pwr_con_W4Ptn32dc3jF9h8h0BW8N3GSF5IGxB7J" %in% ls()))
+{
+	hse_pwr_con_W4Ptn32dc3jF9h8h0BW8N3GSF5IGxB7J <- read.table("./household_power_consumption_W4Ptn32dc3jF9h8h0BW8N3GSF5IGxB7J.txt",
+		na.strings='?', sep=";", quote="", header=TRUE, colClasses = c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric") )
+}
+
+## Data reshaping
+if(!("sub_hse_pwr_con_W4Ptn32dc3jF9h8h0BW8N3GSF5IGxB7J" %in% ls()))
+{	
+	## Preserve the integrity/rawness of hse_pwr_con_W4Ptn32dc3jF9h8h0BW8N3GSF5IGxB7J by changing a variable
+	hse_pwr_con <- hse_pwr_con_W4Ptn32dc3jF9h8h0BW8N3GSF5IGxB7J
+
+	hse_pwr_con$DateTime <- strptime(paste(hse_pwr_con$Date, hse_pwr_con$Time), "%d/%m/%Y %H:%M:%S")
+	Start_Date <- strptime("2007-02-01", "%Y-%m-%d")
+	End_Date <- strptime("2007-02-03", "%Y-%m-%d")
+	sub_hse_pwr_con_W4Ptn32dc3jF9h8h0BW8N3GSF5IGxB7J <- hse_pwr_con[((hse_pwr_con$DateTime >= Start_Date) & (hse_pwr_con$DateTime <= End_Date)),]
+
+	## Preserve the integrity/rawness of sub_hse_pwr_con_W4Ptn32dc3jF9h8h0BW8N3GSF5IGxB7J by changing a variable
+	sub_hse_pwr_con <- sub_hse_pwr_con_W4Ptn32dc3jF9h8h0BW8N3GSF5IGxB7J	
+}
+
+
+## Plotting Plot 4
+png(file="plot4.png")
+par(mfrow=c(2,2))
+#- Top Left -#
+with(sub_hse_pwr_con, plot(DateTime,Global_active_power,type='l',xlab="",ylab='Global Active Power'))
+
+#- Top Right -#
+with(sub_hse_pwr_con, plot(DateTime,Voltage,type='l',xlab="datetime",ylab='Voltage'))
+
+#- Bottom Left -#
+with(sub_hse_pwr_con,  plot(DateTime,Sub_metering_1,type='l',xlab="",ylab='Energy sub metering',col='Black'))
+with(sub_hse_pwr_con, lines(DateTime,Sub_metering_2,col='Red'))
+with(sub_hse_pwr_con, lines(DateTime,Sub_metering_3,col='Blue'))
+legend('topright', legend=paste("Sub_metering_",1:3,sep=""), col=c("Black","Red","Blue"), lty=1, bty='n')
+
+#- Bottom Right -#
+with(sub_hse_pwr_con, plot(DateTime,Global_reactive_power,type='l',xlab="datetime",ylab='Global_reactive_power'))
+dev.off()
+
